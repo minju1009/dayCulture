25a31fcc2019ee6a0681e8ed0e1b08b3b51abf8f
snaps-ariel <ariel@snaps.com>
2023-06-16 18:16:54 +0900

    feat: api 폴더에 healthcheck 추가

--- day-culture/src/pages/api/test.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: 'Server is running' });
-  console.log(res);
-}
