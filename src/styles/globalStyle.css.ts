5032056cbbfa5a2f7c2b2152642176ae366899fa
Minju <minju1009@gmail.com>
2023-06-18 17:59:55 +0900

    chore: 초기세팅 지워진 파일 추가

--- src/styles/globalStyle.css.ts
+++ src/styles/globalStyle.css.ts
@@ -3,8 +3,4 @@ import { globalStyle } from '@vanilla-extract/css';
 globalStyle('html, body', {
   boxSizing: 'border-box',
   fontFamily: 'Segoe UI',
-  color: 'black',
-  padding: 0,
-  margin: 0,
-  backgroundColor: 'pink',
 });
