9c3900463aa59db1fd050220d2bc74f141018f0a
minju1009 <minju1009@gmail.com>
2023-06-19 20:55:57 +0900

    feat: next.config.js에 이미지 출처 추가

--- next.config.js
+++ next.config.js
@@ -1,15 +1,24 @@
 /** @type {import('next').NextConfig} */
-const {
-  createVanillaExtractPlugin
-} = require('@vanilla-extract/next-plugin');
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig = {
   reactStrictMode: true,
-  experimental:{
-    appDir:false
-  }
-}
+  experimental: {
+    appDir: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'culture.seoul.go.kr',
+      },
+      {
+        protocol: 'http',
+        hostname: 'parks.seoul.go.kr',
+      },
+    ],
+  },
+};
 
 module.exports = withVanillaExtract(nextConfig);
-
