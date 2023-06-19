6079a910424ad18258f51343902a6276b26c59cd
Minju <minju1009@gmail.com>
2023-06-19 10:49:25 +0900

    feat: 사용하지 않는 파일들 삭제

--- src/pages/Entry/styles.css.ts
+++
@@ -1,9 +0,0 @@
-import { style } from '@vanilla-extract/css';
-export const container = style({
-  backgroundColor: 'blue',
-  color: 'white',
-});
-
-export const myComponent = style({
-  backgroundColor: 'rebeccapurple',
-});
